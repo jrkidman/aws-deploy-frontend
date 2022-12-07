ebdbed720de48081e43bb2b30fd308bb5d874e60
Jill Kidman <jillkidman@gmail.com>
2022-12-07 10:46:47 -0600

    changed port in axios.js

--- src/lib/Axios.js
+++ src/lib/Axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const Axios = axios.create({
-    baseURL: process.env.REACT_APP_AXIOS === 'development' ? 'http://localhost:3001/api' : '/api',
+    baseURL: process.env.REACT_APP_AXIOS === 'development' ? 'http://localhost:4000/api' : '/api',
     timeout: 50000
 })
 
